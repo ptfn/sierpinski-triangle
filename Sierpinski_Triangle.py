24b143c1f2af0d7b2c2fd59d1a5e2e6bdc8bcbdc
ptfn <CipherDogs@protonmail.com>
2022-01-09 17:54:11 +0700

    new code

--- Sierpinski_Triangle.py
+++ Sierpinski_Triangle.py
@@ -1,81 +1,66 @@
-from tkinter import Tk, Canvas, Frame, BOTH
+from PIL import Image, ImageColor
+from PIL import ImageDraw
 import random
-import sys
+
 
 class Point():
-  def __init__(self, x, y, color):
-    self.x = x
-    self.y = y
-    self.color = color
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
 
-  def paint(self, canvas):
-    canvas.create_rectangle(self.x, self.y, self.x + 1, self.y + 1, outline = self.color, width = 0.5)
- 
-class Example(Frame):
+    def paint(self, draw):
+        draw.point((self.x, self.y), fill=ImageColor.getrgb(self.color))
+
+
+class Triangle():
+    def __init__(self):
+        self.width = 1280
+        self.height = 1024
+        self.image = Image.new("RGB", (self.width, self.height))
+        self.draw = ImageDraw.Draw(self.image)
   
-    def __init__(self, parent):
-        Frame.__init__(self, parent , background = "white")   
-        self.parent = parent        
-        self.initUI()     
-    
-    def initUI(self):
-        self.parent.title("Sierpinski Triangle")        
-        self.pack(fill=BOTH, expand=1)
-        
-        canvas = Canvas(self)
-        canvas.pack(fill=BOTH, expand=1)
+    def rendering(self):
+        a = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
+        a.paint(self.draw)
+        b = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
+        b.paint(self.draw)
+        c = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
+        c.paint(self.draw)
 
-        a = Point(random.randrange(50, 300), random.randrange(50, 300), "green")
-        a.paint(canvas)
-
-        b = Point(random.randrange(50, 300), random.randrange(50, 300), "green")
-        b.paint(canvas)
-
-        c = Point(random.randrange(50, 300), random.randrange(50, 300), "green")
-        c.paint(canvas)
-
-        first = Point(random.randrange(50, 200), random.randrange(50, 200), "red")
-        first.paint(canvas)
+        first = Point(random.randrange(50, self.height), random.randrange(50, self.width), "red")
+        first.paint(self.draw)
 
         def calcPoint(p1:Point, p2:Point):
-            # if p1.x > p2.x: 
-            #     x = ((p1.x - p2.x) / 2) + p2.x 
-            # else:
-            #     x = ((p2.x - p1.x) / 2) + p1.x
-
-            # if p1.y > p2.y: 
-            #     y = ((p1.y - p2.y) / 2) + p2.y
-            # else:
-            #     y = ((p2.y - p1.y) / 2) + p1.y
             x = (p1.x + p2.x) / 2
             y = (p1.y + p2.y) / 2
             return Point(x,y,"blue")
 
         i = 0
-        num = int(sys.argv[1]) # argument
-        
+        num = 100000
+
         while i < num:
             random_dot = random.randrange(0,300)
           
-            if random_dot > 0 & random_dot < 100:
+            if random_dot > 0 and random_dot < 100:
                 newp = calcPoint(p1 = a, p2 = first)
                 first = newp
-            if random_dot > 101 & random_dot < 200:
+            elif random_dot > 101 and random_dot < 200:
                 newp = calcPoint(p1 = b, p2 = first)
                 first = newp
-            if random_dot > 201 & random_dot < 300:
+            elif random_dot > 201 and random_dot < 300:
                 newp = calcPoint(p1 = c, p2 = first)
                 first = newp
-            newp.paint(canvas)
+            newp.paint(self.draw)
             first = newp
             i = i + 1
-        
+
+        self.image.save("save.png", "PNG")
+
+
 def main():
-    root = Tk()
-    _ex = Example(root)
-    root.configure(bg='white')
-    root.geometry("512x512")
-    root.mainloop()  
+    start = Triangle()
+    start.rendering()
  
 if __name__ == '__main__':
     main()
