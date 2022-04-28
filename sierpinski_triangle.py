b111d3fd4f731f17f989e6788fbee15c83d696f2
ptfn <CipherDogs@protonmail.com>
2022-04-28 17:49:36 +0700

    fixed

--- sierpinski_triangle.py
+++ sierpinski_triangle.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageColor
 from PIL import ImageDraw
 import random
+import time
 
 
 class Point():
@@ -15,16 +16,18 @@ class Point():
 
 class Triangle():
     def __init__(self):
-        self.width = 1280
-        self.height = 1024
+        self.width = 3000
+        self.height = 3000
         self.image = Image.new("RGB", (self.width, self.height))
         self.draw = ImageDraw.Draw(self.image)
   
     def rendering(self):
         a = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
         a.paint(self.draw)
+        
         b = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
         b.paint(self.draw)
+        
         c = Point(random.randrange(0, self.height), random.randrange(0, self.width), "green")
         c.paint(self.draw)
 
@@ -34,26 +37,31 @@ class Triangle():
         def calcPoint(p1:Point, p2:Point):
             x = (p1.x + p2.x) / 2
             y = (p1.y + p2.y) / 2
+            
             return Point(x,y,"blue")
 
         i = 0
-        num = 100000
+        num = 1000000
 
         while i < num:
-            random_dot = random.randrange(0,300)
+            random_dot = random.randrange(0,3000)
           
-            if random_dot > 0 and random_dot < 100:
+            if random_dot >= 0 and random_dot < 1000:
                 newp = calcPoint(p1 = a, p2 = first)
                 first = newp
-            elif random_dot > 101 and random_dot < 200:
+            
+            elif random_dot >= 1000 and random_dot < 2000:
                 newp = calcPoint(p1 = b, p2 = first)
                 first = newp
-            elif random_dot > 201 and random_dot < 300:
+            
+            elif random_dot >= 2000 and random_dot <= 3000:
                 newp = calcPoint(p1 = c, p2 = first)
                 first = newp
+            
             newp.paint(self.draw)
             first = newp
-            i = i + 1
+            i += 1
+            time.sleep(0.00001)
 
         self.image.save("save.png", "PNG")
 
